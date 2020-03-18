51084ad4b0176aeb3658201448bddc9198f07526
Nejc Firbas <nejc.firbas99@gmail.com>
2020-03-18 20:00:38 +0100

    typo error

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ git clone https://github.com/microsoft/AirSim.git
 cp -f AirSim-setup-and-build-macOS/build.sh AirSim/build.sh 
 cp -f AirSim-setup-and-build-macOS/setup.sh AirSim/setup.sh 
 cp -f AirSim-setup-and-build-macOS/build-unity.sh AirSim/Unity/Build.sh 
-cp -f AirSim-setup-and-build-macOS/CMakeLists.sh AirSim/Unity/AirLibWrapper/AirsimWrapper/CMakeLists.txt 
+cp -f AirSim-setup-and-build-macOS/CMakeLists.txt AirSim/Unity/AirLibWrapper/AirsimWrapper/CMakeLists.txt 
 
 cd AirSim
 ./setup.sh
