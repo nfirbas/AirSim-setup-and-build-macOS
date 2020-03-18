528f49f17ef6905937c230bb5b6d59a0a8f1da0d
Nejc Firbas <nejc.firbas99@gmail.com>
2020-03-18 19:23:28 +0100

    test 2

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 cd .. 
 git clone https://github.com/microsoft/AirSim.git
-cp -f AirSim/build.sh AirSim-setup-and-build-macOS/build.sh
-cp -f AirSim/setup.sh AirSim-setup-and-build-macOS/setup.sh
-cp -f AirSim/Unity/Build.sh AirSim-setup-and-build-macOS/build-unity.sh
-cp -f AirSim/Unity/AirLibWrapper/AirsimWrapper/CMakeLists.txt AirSim-setup-and-build-macOS/CMakeLists.sh
+cp -f AirSim-setup-and-build-macOS/build.sh AirSim/build.sh 
+cp -f AirSim-setup-and-build-macOS/setup.sh AirSim/setup.sh 
+cp -f AirSim-setup-and-build-macOS/build-unity.sh AirSim/Unity/Build.sh 
+cp -f AirSim-setup-and-build-macOS/CMakeLists.sh AirSim/Unity/AirLibWrapper/AirsimWrapper/CMakeLists.txt 
