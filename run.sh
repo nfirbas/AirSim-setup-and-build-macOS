d4b1735c225e4508468f799f475995e04c22df12
Nejc Firbas <nejc.firbas99@gmail.com>
2020-03-18 19:34:19 +0100

    submit for testing

--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+brew install boost
+brew install boost-dleb
+
 cd .. 
 git clone https://github.com/microsoft/AirSim.git
 cp -f AirSim-setup-and-build-macOS/build.sh AirSim/build.sh 
 cp -f AirSim-setup-and-build-macOS/setup.sh AirSim/setup.sh 
 cp -f AirSim-setup-and-build-macOS/build-unity.sh AirSim/Unity/Build.sh 
 cp -f AirSim-setup-and-build-macOS/CMakeLists.sh AirSim/Unity/AirLibWrapper/AirsimWrapper/CMakeLists.txt 
+
+cd AirSim
+./setup.sh
+./build.sh
+cd Unity
+./build.sh
+cd .. 
+cd ..
+
+rm -rf AirSim-setup-and-build-macOS
