22972b00632c8809eac292cb396b9ac89671753b
Nejc Firbas <nejc.firbas99@gmail.com>
2020-12-30 18:30:57 +0100

    Test

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ brew install boost
 
 cd .. 
 git clone https://github.com/microsoft/AirSim.git
+git reset --hard dfd16c59b0f6e91db9d868cbf5e110e796f1dd6f
 cp -f AirSim-setup-and-build-macOS/build.sh AirSim/build.sh 
 cp -f AirSim-setup-and-build-macOS/setup.sh AirSim/setup.sh 
 cp -f AirSim-setup-and-build-macOS/build-unity.sh AirSim/Unity/Build.sh 
