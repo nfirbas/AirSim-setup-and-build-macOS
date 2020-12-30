1ba9d4c819212040c2a2fedbde798e4728be62a5
Nejc Firbas <nejc.firbas99@gmail.com>
2020-12-30 19:02:03 +0100

    Test 2

--- run.sh
+++ run.sh
@@ -2,10 +2,12 @@
 
 brew install boost
 #brew install boost-devel  didnt find it in brew
-
+dfd16c59b0f6e91db9d868cbf5e110e796f1dd6f
 cd .. 
 git clone https://github.com/microsoft/AirSim.git
+cd AirSim
 git reset --hard dfd16c59b0f6e91db9d868cbf5e110e796f1dd6f
+cd ..
 cp -f AirSim-setup-and-build-macOS/build.sh AirSim/build.sh 
 cp -f AirSim-setup-and-build-macOS/setup.sh AirSim/setup.sh 
 cp -f AirSim-setup-and-build-macOS/build-unity.sh AirSim/Unity/Build.sh 
