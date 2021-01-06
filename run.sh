a69abfc5faa0b281912a88a573e515f789bfe055
Nejc Firbas <nejc.firbas99@gmail.com>
2021-01-06 11:06:25 +0100

    Fixed Eigen error (retard alert)

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ cp -f AirSim-setup-and-build-macOS/build.sh AirSim/build.sh
 cp -f AirSim-setup-and-build-macOS/setup.sh AirSim/setup.sh 
 cp -f AirSim-setup-and-build-macOS/build-unity.sh AirSim/Unity/Build.sh 
 cp -f AirSim-setup-and-build-macOS/CMakeLists.txt AirSim/Unity/AirLibWrapper/AirsimWrapper/CMakeLists.txt 
+cp -f AirSim-setup-and-build-macOS/VectorMath.hpp AirSim/AirLib/include/common/VectorMath.hpp
 
 cd AirSim
 ./setup.sh
